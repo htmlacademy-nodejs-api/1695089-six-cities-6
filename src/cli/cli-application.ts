5db00500701beec171cec9853e82abd2d87ae5b8
zholud artem <zholud.artem@gmail.com>
2024-01-15 23:38:43 +0200

    Добавил менеджеру метод processCommand в CLIApplication

--- src/cli/cli-application.ts
+++ src/cli/cli-application.ts
@@ -1,10 +1,15 @@
 import {Command} from './commands/command.interface.js';
+import {CommandParser} from './command-parser.js';
 
 
 type CommandCollection = Record<string, Command>
 
 export class CliApplication {
   private commands: CommandCollection = {};
+
+  constructor(
+    private readonly defaultCommand = '--help'
+  ) {}
 
   public registerCommands(commandList: Command[]) {
     commandList.forEach((command) => {
@@ -14,5 +19,24 @@ export class CliApplication {
 
       this.commands[command.getName()] = command;
     });
+  }
+
+  public getCommand(commandName: string): Command {
+    return this.commands[commandName] ?? this.getDefaultPublicCommand();
+  }
+
+  public getDefaultPublicCommand(): Command | never {
+    if (! this.commands[this.defaultCommand]) {
+      throw new Error(`The default command (${this.defaultCommand}) is not registered.`);
+    }
+    return this.commands[this.defaultCommand];
+  }
+
+  public processCommand(argv: string[]) {
+    const parsedCommand = CommandParser.parse(argv);
+    const [commandName] = Object.keys(parsedCommand);
+    const command = this.getCommand(commandName);
+    const commandArguments = parsedCommand[commandName] ?? [];
+    command.execute(...commandArguments);
   }
 }
