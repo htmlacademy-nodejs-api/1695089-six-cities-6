7a0cc09b104fc448a0525d301675f905b81abe4b
zholud artem <zholud.artem@gmail.com>
2024-01-17 00:02:02 +0200

    Зарегистрировал команду ImportCommand

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,10 +1,11 @@
-import {CliApplication, HelpCommand, VersionCommand} from './cli/index.js';
+import {CliApplication, HelpCommand, ImportCommand, VersionCommand} from './cli/index.js';
 
 function bootstrap() {
   const cliApplication = new CliApplication();
   cliApplication.registerCommands([
     new HelpCommand(),
-    new VersionCommand()
+    new VersionCommand(),
+    new ImportCommand(),
   ]);
 
   cliApplication.processCommand(process.argv);
