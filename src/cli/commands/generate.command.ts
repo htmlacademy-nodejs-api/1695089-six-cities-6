a24762b49084b682c0ab4d5caee7e6c245f3de88
zholud artem <zholud.artem@gmail.com>
2024-01-20 18:52:26 +0200

    Добавил запрос для получения данных. WIP

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,12 +1,34 @@
+import got from 'got';
 import {Command} from './command.interface.js';
+import {MockServerData} from '../../shared/types/index.js';
 
 export class GenerateCommand implements Command {
+  private initialData: MockServerData;
+
+  private async load(url: string) {
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      throw new Error(`Can't load data from ${url}`);
+    }
+  }
+
   public getName(): string {
     return '--generate';
   }
 
-  public execute(...parameters: string[]) {
+  public async execute(...parameters: string[]) {
     const [count, filepath, url] = parameters;
     const offerCount = parseInt(count, 10);
+
+    try {
+      await this.load(url);
+    } catch (error: unknown) {
+      console.error('Can\'t generate data');
+
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
   }
 }
