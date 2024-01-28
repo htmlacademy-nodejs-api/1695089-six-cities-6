9723f94044f09b0b1655579f32e11374e3f0c4d9
zholud artem <zholud.artem@gmail.com>
2024-01-28 13:29:37 +0200

    Добавил класс приложения RestApplication

---
+++ src/rest/rest.application.ts
@@ -0,0 +1,11 @@
+import {Logger} from '../shared/libs/logger/index.js';
+
+export class RestApplication {
+  constructor(
+    private readonly logger: Logger
+  ) {}
+
+  public init() {
+    this.logger.info('Application initialization');
+  }
+}
