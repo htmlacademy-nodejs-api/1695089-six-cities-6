520a7b333a5a0687de3422cc9364901e2d8f0899
zholud artem <zholud.artem@gmail.com>
2024-01-29 23:50:01 +0200

    Внедрит зависимости в RestApplication

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,11 +1,15 @@
 import {Logger} from '../shared/libs/logger/index.js';
 import {Config, RestSchema} from '../shared/libs/config/index.js';
+import {injectable, inject} from 'inversify';
+import {Component} from '../shared/types/index.js';
 
+@injectable()
 export class RestApplication {
   constructor(
-    private readonly logger: Logger,
-    private readonly config: Config<RestSchema>
-  ) {}
+    @inject(Component.Logger) private readonly logger: Logger,
+    @inject(Component.Config) private readonly config: Config<RestSchema>
+  ) {
+  }
 
   public init() {
     this.logger.info('Application initialization');
