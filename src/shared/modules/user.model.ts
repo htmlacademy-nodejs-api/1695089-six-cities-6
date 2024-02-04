7f6f2725720348118db1050103de6aed6eb121c5
zholud artem <zholud.artem@gmail.com>
2024-02-04 21:37:28 +0200

    Расширит интерфейс UserDocument. WIP

--- src/shared/modules/user.model.ts
+++ src/shared/modules/user.model.ts
@@ -2,6 +2,8 @@ import {Schema, Document, model} from 'mongoose';
 import {User} from '../types/index.js';
 
 export interface UserDocument extends User, Document {
+  createdAt: Date,
+  updatedAt: Date,
 }
 
 const userSchema = new Schema({
@@ -9,7 +11,7 @@ const userSchema = new Schema({
   email: String,
   avatarPath: String,
   status: String,
-});
+}, {timestamps: true});
 
 
 export const UserModel = model<UserDocument>('User', userSchema);
