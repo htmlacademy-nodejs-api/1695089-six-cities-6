fe941c135b0bb823c8eeb8eca5b3968c37b4165a
zholud artem <zholud.artem@gmail.com>
2024-02-04 23:44:35 +0200

    Добавит имплементацию методов UserService

--- src/shared/modules/user/default-user.service.ts
+++ src/shared/modules/user/default-user.service.ts
@@ -1,5 +1,5 @@
 import {UserService} from './user-service.interface.js';
-import {DocumentType, types } from '@typegoose/typegoose';
+import {DocumentType, types} from '@typegoose/typegoose';
 import {UserEntity} from './user.entity.js';
 import {CreateUserDto} from './dto/create-user.dto.js';
 import {inject, injectable} from 'inversify';
@@ -24,5 +24,19 @@ export class DefaultUserService implements UserService {
     this.logger.info(`New user created: ${user.email}`);
 
     return result;
+  }
+
+  public async findByEmail(email: string): Promise<DocumentType<UserEntity> | null> {
+    return this.userModel.findOne({email});
+  }
+
+  public async findOrCreate(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
+    const existedUser = await this.findByEmail(dto.email);
+
+    if (existedUser) {
+      return existedUser;
+    }
+
+    return this.create(dto, salt);
   }
 }
