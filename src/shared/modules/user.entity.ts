aa9ba4a6dd2fbabd9537626d04ec122ed1e008f1
zholud artem <zholud.artem@gmail.com>
2024-02-04 22:21:57 +0200

    Добавит определение модели и схемы. WIP

--- src/shared/modules/user.entity.ts
+++ src/shared/modules/user.entity.ts
@@ -1,8 +1,18 @@
 import {User, UserStatusType} from '../types/index.js';
+import {getModelForClass, prop} from '@typegoose/typegoose';
 
 export class UserEntity implements User {
+  @prop({type: String, required: true})
   public username: string;
+
+  @prop({type: String, required: true, unique: true})
   public email: string;
+
+  @prop({type: String, required: false})
   public avatarPath?: string;
+
+  @prop({type: String, required: true})
   public status: UserStatusType;
 }
+
+export const UserModel = getModelForClass(UserEntity);
