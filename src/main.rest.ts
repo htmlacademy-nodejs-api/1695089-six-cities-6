016f7ac23ab14205b3871e839465cabfceb7583e
zholud artem <zholud.artem@gmail.com>
2024-02-04 23:39:18 +0200

    Добавит внедрение зависимостей в DefaultUserService. WIP.

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -3,9 +3,13 @@ import {RestApplication} from './rest/index.js';
 import {Container,} from 'inversify';
 import {Component} from './shared/types/index.js';
 import {createRestApplicationContainer} from './rest/index.js';
+import { createUserContainer } from './shared/modules/user/index.js';
 
 async function bootstrap() {
-  const appContainer = Container.merge(createRestApplicationContainer());
+  const appContainer = Container.merge(
+    createRestApplicationContainer(),
+    createUserContainer(),
+  );
 
   const application = appContainer.get<RestApplication>(Component.RestApplication);
   await application.init();
